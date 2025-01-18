64bb1598e6cbecc7319e0fb269bed886b7c3c0a5
dev_pine <142303892+SOCIALPINE@users.noreply.github.com>
2025-01-18 17:47:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask,jsonify
 from llama_cpp import Llama
 app = Flask(__name__)
 bot_prompt = """넌 tensor 라는 이름을 가지고있는 ai 모델이야 대화에 친절하게 대답해."""
-llmaiagent = Llama(model_path="./Llama-3.2-3B-Instruct.gguf", verbose=True, n_ctx=512)
+llmaiagent = Llama(model_path="llamaapi/Llama-3.2-3B-Instruct.gguf", verbose=True, n_ctx=512)
 @app.route('/<path:result>', methods=['GET'])
 def echo_text(result):
     output = llmaiagent(bot_prompt+"\n""chat:"+result+"A:", max_tokens=256, stop=["chat:", "\n","Chat:","A:","B:","C:"], echo=False)
